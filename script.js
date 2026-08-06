dc125d0bf599ae704fe86c16bf187e783be01313
nakulsingal98 <nakulsingal98@gmail.com>
2026-08-05 23:14:50 -0400

    Added smooth scrolling

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const openBtn = document.getElementById("openBtn");
+
+    openBtn.addEventListener("click", () => {
+
+        document.getElementById("welcome").scrollIntoView({
+            behavior: "smooth"
+        });
+
+    });
+
+});
